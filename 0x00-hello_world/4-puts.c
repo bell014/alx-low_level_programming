a813411aa218a7074c8b04af4d37618c6a1d00e5
bell014 <neder10tarroui@gmail.com>
2023-07-13 15:14:52 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Description: A C program that prints with put function.
  *
- * Return Always 0(success)
- */
+ * Return: Always 0(success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
